5956e035a08f3ede39b565a71971a0d83a120288
Benedikt Bergmann <benedikt.bergmann@crmkonsulterna.se>
2020-12-23 00:32:01 +0100

    Adding config page Adding default caption

--- image-config-defaults/image-config-defaults.php
+++ image-config-defaults/image-config-defaults.php
@@ -3,7 +3,7 @@
  * Plugin Name: Image Config Defaults
  * Plugin URI:  https://github.com/BenediktBergmann/WordPress-ImageConfigDefaults-Plugin
  * Description: Adds default configuration to images when added to a blog post within gutenberg editor. It will load the configuration of the option page (image_default_align and image_default_link_type).
- * Version:     1.0.0
+ * Version:     1.2.0
  * Author:      Benedikt Bergmann
  * Author URI:  https://benediktbergmann.eu
  * Text Domain: Auto-Anchor 
@@ -39,6 +39,10 @@
 				$link = "none";
 				break;
 		}
+
+		$options = get_option( 'imageConfigDefaults_plugin_options' );
+		$caption = $options['caption'];
+
 	?>
 		<script>
 		function imageConfigDefaults_setImageDefaultSettings(settings, name) {
@@ -46,8 +50,15 @@
 				return settings;
 			}
 
+			debugger;
+
 			settings.attributes.linkDestination.default = "<?php echo $link ?>";
 			settings.attributes.align.default = "<?php echo $alignment ?>";
+
+			if(!settings.attributes.caption){
+				settings.attributes.caption = {};
+			}
+			settings.attributes.caption.default = "<?php echo $caption ?>";
 
 			return settings;
 		}
@@ -59,5 +70,60 @@
 		);
 		</script>
 	<?php
+	}
+
+	/* Create Settings page */
+	function imageConfigDefaults_add_settings_page() {
+		add_options_page( 'Image Config Default Plugin Settings', 'Image Config Default', 'manage_options', 'imageConfigDefaults', 'imageConfigDefaults_render_plugin_settings_page' );
+	}
+	add_action( 'admin_menu', 'imageConfigDefaults_add_settings_page' );
+
+	function imageConfigDefaults_render_plugin_settings_page() {
+		?>
+		<h2>Image Config Default Plugin Settings</h2>
+		<form action="options.php" method="post">
+			<?php 
+			settings_fields( 'imageConfigDefaults_plugin_options' );
+			do_settings_sections( 'imageConfigDefaults_plugin' ); ?>
+			<input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e( 'Save' ); ?>" />
+		</form>
+		<?php
+	}
+
+	function imageConfigDefaults_register_settings() {
+		register_setting( 'imageConfigDefaults_plugin_options', 'imageConfigDefaults_plugin_options');
+		add_settings_section( 'default_settings', 'Image default settings', 'imageConfigDefaults_plugin_section_text', 'imageConfigDefaults_plugin' );
+	
+		add_settings_field( 'imageConfigDefaults_plugin_setting_align', 'image_default_align', 'imageConfigDefaults_plugin_setting_align', 'imageConfigDefaults_plugin', 'default_settings' );
+		add_settings_field( 'imageConfigDefaults_plugin_setting_link_type', 'image_default_link_type', 'imageConfigDefaults_plugin_setting_link_type', 'imageConfigDefaults_plugin', 'default_settings' );
+		add_settings_field( 'imageConfigDefaults_plugin_setting_caption', 'Default caption', 'imageConfigDefaults_plugin_setting_caption', 'imageConfigDefaults_plugin', 'default_settings' );
+	}
+	add_action( 'admin_init', 'imageConfigDefaults_register_settings' );
+
+	function imageConfigDefaults_plugin_section_text() {
+		?>
+			<p>Here you can set all the options for using the Image config Defaults plugin</p>
+		<?php
+	}
+	
+	function imageConfigDefaults_plugin_setting_caption() {
+		$options = get_option( 'imageConfigDefaults_plugin_options' );
+		?>
+			<input id="imageConfigDefaults_plugin_setting_caption" name="imageConfigDefaults_plugin_options[caption]" type="text" value="<?php echo esc_attr( $options['caption'] ); ?>" />
+        <?php
+	}
+
+	function imageConfigDefaults_plugin_setting_align() {
+		$align = get_option( 'image_default_align' );
+		?>
+			<input id="imageConfigDefaults_plugin_setting_align" name="image_default_align" type="text" value="<?php echo esc_attr( $align ); ?>" />
+        <?php
+	}
+
+	function imageConfigDefaults_plugin_setting_link_type() {
+		$linktype = get_option( 'image_default_link_type' );
+		?>
+			<input id="imageConfigDefaults_plugin_setting_link_type" name="image_default_link_type" type="text" value="<?php echo esc_attr( $linktype ); ?>" />
+        <?php
 	}
 ?>
